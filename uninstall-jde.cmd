cbe7c4601d8b18f823772093d88a882071cc5bb3
PRASHANTH BELATHUR <38864182+pbelathur@users.noreply.github.com>
2018-09-22 15:08:53 -0500

    cleanup scripts and add screen shots

--- uninstall-jde.cmd
+++ uninstall-jde.cmd
@@ -1,11 +1,11 @@
 
 choco feature enable -n allowGlobalConfirmation
 
-choco uninstall jdk8
-choco uninstall maven
-choco uninstall gradle
-choco uninstall -- confirm git.install
-choco uninstall putty.install
+choco uninstall intellijidea-community
+choco uninstall postman
 choco uninstall curl
-choco uninstall --confirm postman
-choco uninstall --confirm intellijidea-community
+choco uninstall putty.install
+choco uninstall git.install
+choco uninstall gradle
+choco uninstall maven
+choco uninstall jdk8
